0b7011c1dafbf8c0e2b36513f4d4bce252f69606
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-06-27 00:58:16 -0600

    Comando para editar una tienda
    se agrega comando para editar una cuenta

---
+++ ExpressSolution.Stores.Handlers/Commands/Store/EditStoreHandler.cs
@@ -0,0 +1,36 @@
+﻿using ExpressSolution.Exceptions;
+using ExpressSolution.Stores.Comands.Store;
+using ExpressSolution.Stores.Data;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExpressSolution.Stores.Handlers.Commands.Store
+{
+    class EditStoreHandler : IRequestHandler<EditStore>
+    {
+        private readonly IStoreRepo _storeRepo;
+        public EditStoreHandler(
+            IStoreRepo storeRepo)
+        {
+            _storeRepo = storeRepo;
+        }
+        public async Task<Unit> Handle(EditStore request, CancellationToken cancellationToken)
+        {
+            ExpressSolution.Stores.Store store = await _storeRepo.GetById(request.Id);
+
+            if(store==null)
+                throw ClientException.CreateException(ClientExceptionType.InvalidOperation, nameof(request), this.GetType(), $"La tienda consultada no existe");
+
+            store.Name = request.StoreData.Name;
+            store.Description = request.StoreData.Description with { };
+
+            await _storeRepo.SaveChangesAsync();
+            return new Unit();
+        }
+    }
+}
