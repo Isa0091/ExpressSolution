1ed874bae27598db59c7ca2332aa8fd6369b0704
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-06-26 23:20:33 -0600

    handler para crear category
    se crea handler de creacion de categoria

---
+++ ExpressSolution.Stores.Handlers/Commands/Category/CreateCategoryHandler.cs
@@ -0,0 +1,47 @@
+﻿using ExpressSolution.Stores.Comands.Category;
+using ExpressSolution.Stores.Data;
+using ManagerFileEasyAzure;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExpressSolution.Stores.Handlers.Commands.Category
+{
+    class CreateCategoryHandler : IRequestHandler<CreateCategory>
+    { 
+        private readonly IManagerFileEasyAzureProvider _managerFileEasyAzureProvider;
+        private readonly ICategoryRepo _categoryRepo;
+
+        CreateCategoryHandler(
+            IManagerFileEasyAzureProvider managerFileEasyAzureProvider,
+            ICategoryRepo categoryRepo)
+        {
+            _managerFileEasyAzureProvider = managerFileEasyAzureProvider;
+            _categoryRepo = categoryRepo;
+        }
+
+        public async Task<Unit> Handle(CreateCategory request, CancellationToken cancellationToken)
+        {
+
+           Uri urlImage= await _managerFileEasyAzureProvider.PostFileStorageAsync(request.CategoryData.Multimedia.ToArray(), request.CategoryData.MultimediaName, request.CategoryData.MimeType);
+
+            ExpressSolution.Stores.Category Category = new ExpressSolution.Stores.Category()
+            {
+                 Active= true,
+                 Description= request.CategoryData.Description with { },
+                 Id= request.Id,
+                 Logo= new MultimediaVo(request.CategoryData.MultimediaName, request.CategoryData.MimeType, urlImage.ToString(), MultimediaType.Image),
+                 Name= request.CategoryData.Name
+            };
+
+            await _categoryRepo.AddAsync(Category);
+            await _categoryRepo.SaveChangesAsync();
+
+            return new Unit();
+        }
+    }
+}
---
+++ ExpressSolution/Stores/Dtos/Category/CategoryData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressSolution.Stores.Dtos.Category
+{
+    public class CategoryData
+    {
+        /// <summary>
+        /// Nombre de la categoria
+        /// </summary>
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string MultimediaName { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string MimeType { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<byte> Multimedia { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public MultimediaType MultimediaType { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DescriptionVo Description { get; set; }
+    }
+}
