bb5d36e4a59684c9fe4831eb07fac89ca50a8c81
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-06-27 00:54:11 -0600

    Comand o para crear tienda

---
+++ ExpressSolution.Stores.Handlers/Commands/Store/CreateStoreHandler.cs
@@ -0,0 +1,37 @@
+﻿using ExpressSolution.Stores.Comands.Store;
+using ExpressSolution.Stores.Data;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExpressSolution.Stores.Handlers.Commands.Store
+{
+    class CreateStoreHandler : IRequestHandler<CreateStore>
+    {
+        private readonly IStoreRepo _storeRepo;
+        public CreateStoreHandler(
+            IStoreRepo storeRepo)
+        {
+            _storeRepo = storeRepo;
+        }
+        public async Task<Unit> Handle(CreateStore request, CancellationToken cancellationToken)
+        {
+            ExpressSolution.Stores.Store store = new Stores.Store()
+            {
+                Active = true,
+                Description = request.StoreData.Description with { },
+                Name = request.StoreData.Name,
+                Id = request.Id
+
+            };
+
+            await _storeRepo.AddAsync(store);
+            await _storeRepo.SaveChangesAsync();
+            return new Unit();
+        }
+    }
+}
