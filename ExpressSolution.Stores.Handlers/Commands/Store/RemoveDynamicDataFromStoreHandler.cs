0741e70e62c4d5ac7aee6a9bfa3a120c4c277140
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-06-27 01:50:59 -0600

    Create RemoveDynamicDataFromStoreHandler.cs

---
+++ ExpressSolution.Stores.Handlers/Commands/Store/RemoveDynamicDataFromStoreHandler.cs
@@ -0,0 +1,35 @@
+﻿using ExpressSolution.Exceptions;
+using ExpressSolution.Stores.Comands.Store;
+using ExpressSolution.Stores.Data;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExpressSolution.Stores.Handlers.Commands.Store
+{
+    class RemoveDynamicDataFromStoreHandler : IRequestHandler<RemoveDynamicDataFromStore>
+    {
+        private readonly IStoreRepo _storeRepo;
+        public RemoveDynamicDataFromStoreHandler(
+            IStoreRepo storeRepo)
+        {
+            _storeRepo = storeRepo;
+        }
+        public async Task<Unit> Handle(RemoveDynamicDataFromStore request, CancellationToken cancellationToken)
+        {
+            ExpressSolution.Stores.Store store = await _storeRepo.GetById(request.StoreId);
+
+            if (store == null)
+                throw ClientException.CreateException(ClientExceptionType.InvalidOperation, nameof(request), this.GetType(), $"La tienda consultada no existe");
+
+            store.RemoveDynamicData(request.DataName);
+            await _storeRepo.SaveChangesAsync();
+
+            return new Unit();
+        }
+    }
+}
