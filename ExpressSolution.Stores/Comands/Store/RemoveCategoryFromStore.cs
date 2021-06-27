738d9ddfd092674f2cfb6700dac9ad8ace25260b
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-06-26 22:50:09 -0600

    creacion de comands
    se crean comands sol oclases para agregar remover  categorias de una tienda

---
+++ ExpressSolution.Stores/Comands/Store/RemoveCategoryFromStore.cs
@@ -0,0 +1,13 @@
+﻿using Isa0091.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressSolution.Stores.Comands.Store
+{
+    public class RemoveCategoryFromStore : CommandBase
+    {
+    }
+}
