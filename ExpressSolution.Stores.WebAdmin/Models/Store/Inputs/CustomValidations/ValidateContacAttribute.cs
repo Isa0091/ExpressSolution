999af82b478ba4fc7e0532773a9dcceeaaaccf27
Isa0091 <34584720+Isa0091@users.noreply.github.com>
2021-07-03 21:17:16 -0600

    Recursos para agregar un contacto

---
+++ ExpressSolution.Stores.WebAdmin/Models/Store/Inputs/CustomValidations/ValidateContacAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressSolution.Stores.WebAdmin.Models.Store.Inputs.CustomValidations
+{
+    public class ValidateContacAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            StoreContactInputVm contacto = (StoreContactInputVm)validationContext.ObjectInstance;
+
+            if (string.IsNullOrEmpty(contacto.Email) && string.IsNullOrEmpty(contacto.MobileNumber) && string.IsNullOrEmpty(contacto.LandLineNumber))
+            {
+                return new ValidationResult("Debe de ingresar al menos un contacto: teléfono fijo, celular o email", new[] { nameof(contacto.LandLineNumber) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ ExpressSolution.Stores.WebAdmin/Models/Store/Inputs/StoreContactInputVm.cs
@@ -0,0 +1,49 @@
+﻿using ExpressSolution.Stores.WebAdmin.Models.Store.Inputs.CustomValidations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressSolution.Stores.WebAdmin.Models.Store.Inputs
+{
+    public class StoreContactInputVm
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string StoreId { get; set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ContactId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [StringLength(300, ErrorMessage = "La longitud del nombre debe ser {1}")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [ValidateContacAttribute]
+        [StringLength(50, ErrorMessage = "La longitud la linea fija debe ser {1}")]
+        public string LandLineNumber { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string MobileNumber { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [EmailAddress(ErrorMessage = "El formato del email es incorrecto")]
+        [StringLength(200, ErrorMessage = "La longitud del email debe ser {1}")]
+        public string Email { get; set; }
+    }
+}
